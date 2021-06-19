019c309c9382206bd6a461eb080daf8a0825dd1f
Subhransu Sekhar Das <37026826+subhransu1@users.noreply.github.com>
2021-06-19 11:43:33 +0530

    Delete documentweb directory

--- documentweb/src/main/java/com/Bhran/documentweb/repository/DocumentRepo.java
+++
@@ -1,8 +0,0 @@
-package com.Bhran.documentweb.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.Bhran.documentweb.entities.Document;
-
-public interface DocumentRepo extends JpaRepository<Document, Long>{
-
-}
